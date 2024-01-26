ecaf70a30c9fb029bdd62f4881069c6756c9cdd3
Akshith Reddy <akshith@Akshiths-MacBook-Pro.local>
2024-01-25 22:34:08 -0500

    Initial commit

---
+++ DSA_Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int[] arr = new int[20];
+arr[0] = 12;
+arr[1] = 16;
+arr[2] = 20;
+arr[3] = 40;
+arr[4] = 50;
+arr[5] = 70;
+int n = 6;
+for (int i=0; i<n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+int key = 80;
+
+
+n = insertElement(arr, key, n);
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
+static int insertElement(int[] arr, int key, int n)
+{
+    if (n > arr.Length - 1)
+        return n;
+
+    arr[n] = key;
+    return n + 1;
+
+}
